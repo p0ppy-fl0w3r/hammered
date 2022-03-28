ad3c3fdb7b06321a43610330a6b20388eb1cca1c
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2022-03-28 21:43:12 +0545

    Added attributation fragment.

---
+++ app/src/main/java/com/fl0w3r/hammered/about/AttributeFragment.kt
@@ -0,0 +1,27 @@
+package com.fl0w3r.hammered.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.fl0w3r.hammered.R
+import com.fl0w3r.hammered.databinding.FragmentAboutBinding
+import com.fl0w3r.hammered.databinding.FragmentHammerMeBinding
+
+
+class AttributeFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_attribute, container, false)
+        return binding.root
+
+    }
+
+}
